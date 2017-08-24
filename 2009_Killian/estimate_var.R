cce00831a8c696d32f2553b217e6eb5316fb4bff
CommonEconomist <weezel.van@gmail.com>
2017-08-24 14:52:53 +0200

    update Kilian

--- 2009_Killian/estimate_var.R
+++ 2009_Killian/estimate_var.R
@@ -1,6 +1,8 @@
 ## Attempting to replicate "Not all oil price shocks are alike"
 # Killian (2009)
 # https://www.aeaweb.org/articles?id=10.1257/aer.99.3.1053
+# For 'vars' package see:
+# http://ftp.uni-bayreuth.de/math/statlib/R/CRAN/doc/vignettes/vars/vars.pdf
 # In progress
 setwd("~/Dropbox/github/replications/2009_Killian")
 
@@ -13,22 +15,27 @@ plot(x,bty="n")
 
 #### Estimate model: vars package ####
 require(vars)
-m<-VAR(x,p=24) # 24 lags
+m<-VAR(x,p=24) # 24 lags, reduced-form VAR
 
-# Impulse response functions, takes about three minutes each
-d.prod.i<-irf(m,impulse="d.prod",ci=.95,runs=1000,seed=42,n.ahead=15) 
-rea.i<-irf(m,impulse="rea",ci=.95,runs=1000,seed=42,n.ahead=15) 
-rpo.i<-irf(m,impulse="rpo",ci=.95,runs=1000,seed=42,n.ahead=15) 
+# For IRF compute SVAR
+Am=diag(3)
+Am[2:3,1]<-NA
+Am[3,2]<-NA
+
+s<-SVAR(m,estmethod="direct",Amat=Am,Bmat=NULL,hessian=TRUE,method="BFGS")
+
+# Impulse response functions; can take a couple of minutes
+d.prod.i<-irf(s,impulse="d.prod",ci=.95,runs=1000,seed=42,n.ahead=15) 
+rea.i<-irf(s,impulse="rea",ci=.95,runs=1000,seed=42,n.ahead=15) 
+rpo.i<-irf(s,impulse="rpo",ci=.95,runs=1000,seed=42,n.ahead=15) 
 
 # Examine the IRFs; compare with fig.3 from paper.
-plot(d.prod.i) # Way off
-plot(rea.i) # Slight resemblance
-plot(rpo.i) # Slight resemblance
+plot(d.prod.i) 
+plot(rea.i) 
+plot(rpo.i) 
 
-#### Estimate model: MBSVAR package ####
-detach("package:vars",unload=TRUE)
-library(MSBVAR)
-m2<-reduced.form.var(x,p=24)
-m2.irf<-irf(m2,nsteps=24)
-
-plot(m2.irf) # Same IRFs, little resemblance for oil supply shocks, or others
+## IRFs do not really correspond to what is reported in the paper. 
+# Kilian estimates a VAR model with OLS and uses the estimates to construct
+# the structural VAR representation of the model which is used to calculate
+# the IRFs. However, some detail is lacking and so far I have been unable to 
+# replicate the result.
