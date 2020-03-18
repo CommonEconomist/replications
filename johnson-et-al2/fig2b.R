71069be79a5bf7e56ed77bf262abce9139300ebc
CommonEconomist <vanweezel@protonmail.com>
2020-03-18 17:52:12 +0100

    Fig.2

--- johnson-et-al2/fig2b.R
+++ johnson-et-al2/fig2b.R
@@ -1,9 +1,9 @@
 #date created 2020.03.10
-#last update  2020.03.10
+#last update  2020.03.14
 library(brms)
 library(data.table)
 library(tidyverse)
-setwd("~/github/johnson-et-al2")
+setwd("~/github/replications/johnson-et-al2")
 
 #data
 X <- list.files(pattern = "*.csv") %>% 
@@ -17,20 +17,22 @@ d<-data.table(event.date = as.Date(x$Date, format = "%d-%b-%y"),
 
 #variables
 d<-d[order(d$location, d$event.date),]
-d[, next.event := shift(event.date, n=1, type="lead"), by = "where"]
+d[, next.event := shift(event.date, n=1, type="lead"), by = "location"]
 d[, interval := as.numeric(next.event - event.date),]
 d[, event.number := seq_len(.N), by = 'location']
 d<-d[d$interval != 0,]
-d[, `:=`(log.interval = log10(interval), log.event.number = log10(event.number))]
+d[, `:=`(log.interval = log10(interval), 
+         log.event.number = log10(event.number))]
 
 #estimates
 m<-brm(log.interval ~ log.event.number + (1 + log.event.number | location), d)
 beta<-ranef(m, pars = c("log.event.number", "Intercept"))
 
-r<-data.table(where = names(beta[[1]][,,1][,1]),
+r<-data.table(location = names(beta[[1]][,,1][,1]),
               intercept = beta[[1]][,,1][,1],
               estimate = -beta[[1]][,,2][,1])
 
 #plot
+par(pty="s")
 plot(estimate ~ intercept, r)
 abline(lm(estimate ~ intercept, r))
