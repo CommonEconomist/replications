8fc6100587419e216b3140d3c399f110d4e226fb
CommonEconomist <vanweezel@protonmail.com>
2020-05-28 14:31:47 +0200

    adjust code: new Bayesian package

--- stock-watson/vector-autoregression.R
+++ stock-watson/vector-autoregression.R
@@ -1,6 +1,4 @@
-## Replication "Vector Autoregressions"
-# Stock & Watson (2001)
-
+# Replication Stock & Watson (2001). "Vector Autoregressions".
 # Given that the provided replication data for this paper is only available 
 # in a RATS data file, the data needs to be build from scratch following
 # the description in the paper. 
@@ -9,88 +7,65 @@
 # https://fred.stlouisfed.org/series/UNRATE
 # https://fred.stlouisfed.org/series/FEDFUNDS 
 # Only the interest rate is not seasonally adjusted
-setwd("~/github/replications/2001_Stock_Watson")
+setwd("~/github/replications/stock-watson")
 
-#### Prepare data ####
-# NB - Can probably download this data using quantmod package
-require(dplyr)
+#prepare data
+# NB - Can probably download data using quantmod package
+library(data.table)
 
-# Inflation rate
-d<-read.csv("GDPCTPI.csv",stringsAsFactors=FALSE)
-d$gdp.l<-lag(d[,2],lag=1)
-d$pi=400*(log(d[,2]/d[,3]))
+#gdp
+x <- fread("GDPCTPI.csv") 
+x[, L.gdpctpi := shift(GDPCTPI, n = 1, type = "lag") ]
+x[, pi := 400 * (log(GDPCTPI/L.gdpctpi))]
 
-pi<-ts(d$pi,start=c(1947,1),frequency=4)
-pi<-window(pi,start=c(1960,1),end=c(2000,4))
+pi <- ts(x$pi, start = c(1947, 1), frequency = 4)
+pi <- window(pi,start = c(1960,1), end=c(2000,4))
 
-# Unemployment rate
-d<-read.csv("UNRATE.csv",stringsAsFactors=FALSE)
-u<-ts(d$UNRATE,start=c(1948,1),frequency=12)
-u<-aggregate(u,nfrequency=4,FUN=mean)
+#unemployment
+x <- fread("UNRATE.csv")
 
-u<-window(u,start=c(1960,1),end=c(2000,4))
+u <- ts(x$UNRATE, start = c(1948,1), frequency=12)
+u <- aggregate(u, nfrequency = 4, FUN = mean)
+u <- window(u, start = c(1960,1), end = c(2000,4))
 
-# Interest rate
-d<-read.csv("FEDFUNDS.csv",stringsAsFactors=FALSE)
-R<-ts(d$FEDFUNDS,start=c(1954,7),frequency=12)
-R<-aggregate(R,nfrequency=4,FUN=mean)
+#interest rate
+x <- fread("FEDFUNDS.csv")
+r <- ts(x$FEDFUNDS, start = c(1954,7), frequency = 12) 
+r <- aggregate(r, nfrequency = 4, FUN = mean)
+r <- window(r, start = c(1960,1), end = c(2000,4))
 
-R<-window(R,start=c(1960,1),end=c(2000,4))
+x <- cbind(pi,u,r) #combine
 
-x<-cbind(pi,u,R) # Combine data
+#plot data
+par(mar=c(5,5,2,2), mfrow=c(3,1), las=1, bty="n")
+plot(pi, axes=FALSE, xlab="",ylab="",main="Inflation",lwd=2)
+axis(2,tick=FALSE)
 
-#### Plot data ####
-par(mar=c(5,5,2,2),mfrow=c(3,1),las=1,bty="n",cex.lab=2,cex.axis=2,cex.main=2)
-plot(pi,axes=FALSE,xlab="",ylab="",main="Inflation",lwd=2);axis(2,tick=FALSE)
-plot(u,axes=FALSE,xlab="",ylab="",main="Unemployment",lwd=2);axis(2,tick=FALSE)
-plot(R,axes=FALSE,xlab="",ylab="",main="Interest rate",lwd=2)
+plot(u,axes=FALSE,xlab="",ylab="",main="Unemployment",lwd=2)
+axis(2,tick=FALSE)
+
+plot(r,axes=FALSE,xlab="",ylab="",main="Interest rate",lwd=2)
 axis(1,tick=FALSE);axis(2,tick=FALSE)
 
-#### Estimate model ####
+#fit model
 library(vars)
-m<-VAR(x,p=4) # Fit VAR with 4 lags
+m <- VAR(x, p = 4) #VAR with 4 lags
 
-# Impulse response functions (takes a couple of minutes)
-pi.i<-irf(m,impulse="pi",ci=.95,runs=1000,seed=42,n.ahead=24) # Inflation
-u.i<-irf(m,impulse="u",ci=.95,runs=1000,seed=42,n.ahead=24)   # Unemployment
-R.i<-irf(m,impulse="R",ci=.95,runs=1000,seed=42,n.ahead=24)   # Interest rate
+#impulse response functions (NB - takes some seconds)
+irf.pi <- irf(m, impulse = "pi", ci = .5, runs=1000,seed=42, n.ahead=24) 
+irf.u <- irf(m, impulse = "u", ci = .5, runs=1000,seed=42, n.ahead=24) 
+irf.r <- irf(m, impulse = "r", ci = .5, runs=1000,seed=42, n.ahead=24) 
 
-# Plot results
-# NB - Having difficulties in changing the plot lay out
-# Shape of IRF is similar to those reported in paper, only some minor 
-# differences here and there in estimated magnitude. 
+#plot (NB - difficulties with adjusting lay-out)
 par(pty="s",bty="n")
-plot(pi.i)
-plot(u.i)
-plot(R.i)
+plot(irf.pi)
+plot(irf.u)
+plot(irf.r)
 
-#### Test with Bayesian VAR estimation ####
-# NB - better options for granger causality test and variance decomposition
-detach("package:vars",unload=TRUE)
-require(MSBVAR)
-m<-reduced.form.var(x,4,z=NULL) # Estimate model, 4 lags
+#^similar to figure 1 in paper; only small changes in magnitude.
 
-# Test Granger causality
-granger.test(x,4) # Results far off from those reported in table 1 panel a
-
-# Variance decomposition
-dfev(m,k=12) # Again, substantial differences
-
-# Impulse response function
-m.irf<-irf(m,nsteps=24) # 24 steps ahead
-plot(m.irf) # Strangely pretty much as in the paper.
-
-# Try to get uncertainty level with MC simulation
-m.irf2<- mc.irf(m,nsteps=24, draws=2000)
-plot(m.irf2,method="Percentile",probs=c(.16,.84)) # All over the place
-
-m.irf2<-mc.irf(m,nsteps=4,draws=2000) # Reduce forecast horizon
-plot(m.irf2,method="Percentile",probs=c(.16,.84)) # Problem persists
-
-# BVAR model
-# NB - mc.irf gives same weird IRF plot
-m.bvar<-szbvar(x,p=4,z=NULL,
-                 lambda0=1,lambda1=1,lambda3=1,lambda4=1,lambda5=0,
-                mu5=1,mu6=1,nu=4,qm=4,prior=1, posterior.fit=TRUE)
-plot(irf(m.bvar,nsteps=24))
-
+#Bayesian var
+library(BVAR)
+m <- bvar(x, lags = 4)
+imp.response <- irf(m, bv_irf(horizon = 24L))
+plot(imp.response)
